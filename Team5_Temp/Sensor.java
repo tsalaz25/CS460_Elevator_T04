b920c8815d1bcf2c0a8405b7487aff0c30c64c3a
Jay Megill <jmegill7@unm.edu>
2025-10-20 19:57:27 -0600

    Created an API for the Movement sim sensors, and a temp file for team 5

---
+++ Team5_Temp/Sensor.java
@@ -0,0 +1,28 @@
+public class Sensor{
+    float position;
+    boolean isActive = false;
+    int floor;
+    boolean isRoof;
+
+    Sensor(float position,int floor,boolean isRoof){
+        this.position = position;
+        this.floor = floor;
+        this.isRoof = isRoof;
+    }
+
+    //I don't know if we're doing this in here or in the main function but i putting something here anway
+    public void setActive(float position){
+        //This likely won't work
+        if (position == this.position){
+            this.isActive = true;
+        } else {
+            this.isActive = false;
+        }
+    }
+
+        public void setActive(boolean isActive){
+            this.isActive = isActive;
+        }
+    }
+   
+}
---
+++ Team5_Temp/SensorAPI.java
@@ -0,0 +1,12 @@
+public class SensorAPI{
+    private boolean[] sensors;
+
+    SensorAPI(boolean[] sensors){
+        this.sensors = sensors;
+    }
+
+    public boolean pollSensor(int index){
+        boolean sensor = sensors[index];
+        return sensor;
+    }
+}
