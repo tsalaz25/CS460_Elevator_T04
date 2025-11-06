a976fa134d7d2565e369d43886df3f3d907439ad
Jay Megill <jmegill7@unm.edu>
2025-11-06 15:00:18 -0700

    Updated topics.java to subscribe to some topics and publish others

--- src/Multiplexer/Topics.java
+++ src/Multiplexer/Topics.java
@@ -1,26 +1,35 @@
 package Multiplexer;
 
 import SoftwareBus.Bus.Bus;
+import SoftwareBus.Bus.Message;
 import SoftwareBus.Bus.Topic;
 
 public class Topics {
-    public static final Topic CAR_REQUEST = new Topic(0, 5);
-    public static final Topic ELEVATOR_FLOOR = new Topic(1, 1) ;
-    public static final Topic ELEVATOR_DIRECTION = new Topic(2, 1) ;
-    public static final Topic ELEVATOR_DOOR = new Topic(3, 1) ;
-    public static final Topic ELEVATOR_OVERWEIGHT  = new Topic(4, 1) ;
-    public static final Topic FIRE_KEY = new Topic(6,1);
-    public static final Topic ELEVATOR_STATE = new Topic(7,1);
+    //Device outputs (Publish)
+    //ints are per elevator and need the ID argument
+    public static final int ELEVATOR_FLOOR = 1;
+    public static final int ELEVATOR_DIRECTION = 2;
+    public static final int ELEVATOR_DOOR = 3;
+    public static final int ELEVATOR_OVERWEIGHT  = 4;
+    public static final int FLOOR_SELECT = 8;
 
+    //Bus Inputs (Subscribe)
+    public static final Topic CAR_REQUEST = new Topic(0, 0);
+    public static final int DOOR_COMMAND = 9;
+    public static final int MOTOR_COMMAND = 10;
+        public static final int CABIN_BUTTON_RESET = 11;
+    public static final Topic CALL_BUTTON_RESET = new Topic(12, 0);
 
-    public static void subscribeAll(Bus bus) {
+    // Subscribes to all topics that the multiplexer needs to subscribe to
+    public static void subscribeAll(Bus bus,int ID) {
         bus.subscribe(CAR_REQUEST);
-        bus.subscribe(ELEVATOR_FLOOR);
-        bus.subscribe(ELEVATOR_DIRECTION);
-        bus.subscribe(ELEVATOR_DOOR);
-        bus.subscribe(ELEVATOR_OVERWEIGHT);
-        bus.subscribe(FIRE_KEY);
-        bus.subscribe(ELEVATOR_STATE);
+        bus.subscribe(new Topic(DOOR_COMMAND,ID));
+        bus.subscribe(new Topic(MOTOR_COMMAND,ID));
+        bus.subscribe(CALL_BUTTON_RESET);
+        bus.subscribe(new Topic(CABIN_BUTTON_RESET,ID));
+    }
 
+    public static void publish(Bus bus,Topic t,int[] body){
+        bus.publish(new Message(t,body));
     }
 }
